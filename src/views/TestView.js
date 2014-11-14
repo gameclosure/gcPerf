f4ed75b5c419ede92676c9de454c05476a7f22a9
Jimmy Griffith <rogueSkib@gmail.com>
2014-11-14 15:31:48 -0800

    performance test for binding functions on devkit2

--- src/views/TestView.js
+++ src/views/TestView.js
@@ -1,124 +1,90 @@
+import ui.View as View;
+
+import src.lib.utils as utils;
 import src.views.BaseTestView as BaseTestView;
-import src.conf.enums as enums;
+var rollInt = utils.rollInt;
 
-from src.conf.enums import *;
+var LOOP_ITERS = 1000;
 
-var rand2 = Math.random;
+var Class0 = Class(function () {
+	this.init = function (opts) {};
 
-var Class0 = Class(function() {
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		for (var i = 0; i < 10; i++) {
-			this.stuff += rand0();
+	var b = bind;
+	this.reset = function(fn, ctx) {
+		var f = b(ctx, fn);
+		for (var i = 0; i < LOOP_ITERS; i++) {
+			f(i);
 		}
-		return this.stuff;
 	};
 });
 
-var Class1 = Class(function() {
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		for (var i = 0; i < 10; i++) {
-			this.stuff += rand1();
+var Class1 = Class(function () {
+	this.init = function (opts) {};
+
+	this.reset = function(fn, ctx) {
+		var f = fn.bind(ctx);
+		for (var i = 0; i < LOOP_ITERS; i++) {
+			f(i);
 		}
-		return this.stuff;
 	};
 });
 
-var Class2 = Class(function() {
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		for (var i = 0; i < 10; i++) {
-			this.stuff += rand2();
+var Class2 = Class(function () {
+	this.init = function (opts) {};
+
+	this.reset = function(fn, ctx) {
+		var f = function(i) { fn.call(ctx, i); };
+		for (var i = 0; i < LOOP_ITERS; i++) {
+			f(i);
 		}
-		return this.stuff;
 	};
 });
 
-var Class3 = Class(function() {
-	var rand3 = Math.random;
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		for (var i = 0; i < 10; i++) {
-			this.stuff += rand3();
+var Class3 = Class(function () {
+	this.init = function (opts) {};
+
+	this.reset = function(fn, ctx) {
+		for (var i = 0; i < LOOP_ITERS; i++) {
+			fn.call(ctx, i);
 		}
-		return this.stuff;
 	};
 });
 
-var Class4 = Class(function() {
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		var rand4 = Math.random;
-		for (var i = 0; i < 10; i++) {
-			this.stuff += rand4();
-		}
-		return this.stuff;
-	};
-});
-
-var Class5 = Class(function() {
-	this.init = function() {
-		this.stuff = 0;
-	};
-	this.reset = function() {
-		for (var i = 0; i < 10; i++) {
-			this.stuff += enums.rand1();
-		}
-		return this.stuff;
-	};
-});
+var inst0 = new Class0();
+var inst1 = new Class1();
+var inst2 = new Class2();
+var inst3 = new Class3();
 
 var TestView = exports = Class(BaseTestView, function(supr) {
 	this.init = function(opts) {
 		opts.hideViews = false;
-		opts.disableLogs = false;
+		opts.disableLogs = true;
 		opts.useMicroseconds = true;
 
 		opts.setupFunc = function() {};
 
-		opts.testIters = 10000;
+		opts.testIters = 50;
 
 		opts.testFuncs = [
 			function() {
-				var obj = new Class0();
-				obj.reset();
-				return obj;
+				inst0.reset(function(i) {
+					return i * i;
+				}, this);
 			},
 			function() {
-				var obj = new Class1();
-				obj.reset();
-				return obj;
+				inst1.reset(function(i) {
+					return i * i;
+				}, this);
 			},
 			function() {
-				var obj = new Class2();
-				obj.reset();
-				return obj;
+				inst2.reset(function(i) {
+					return i * i;
+				}, this);
 			},
 			function() {
-				var obj = new Class3();
-				obj.reset();
-				return obj;
-			},
-			function() {
-				var obj = new Class4();
-				obj.reset();
-				return obj;
-			},
-			function() {
-				var obj = new Class5();
-				obj.reset();
-				return obj;
+				inst3.reset(function(i) {
+					return i * i;
+				}, this);
 			}
 		];
 
