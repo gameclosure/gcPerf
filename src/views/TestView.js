fe284699b479322f1f65613a7953c512664e2dea
Jimmy Griffith <rogueSkib@gmail.com>
2016-02-26 18:13:30 -0800

    animate testing :

--- src/views/TestView.js
+++ src/views/TestView.js
@@ -1,90 +1,63 @@
 import ui.View as View;
 
-import src.lib.utils as utils;
+import src.lib.animate1 as animate1;
+import src.lib.animate2 as animate2;
 import src.views.BaseTestView as BaseTestView;
-var rollInt = utils.rollInt;
 
-var LOOP_ITERS = 1000;
+var TestView = exports = Class(BaseTestView, function (supr) {
+	this.init = function (opts) {
+		var view1 = new View({
+			parent: GC.app.view,
+			width: 20,
+			height: 20
+		});
 
-var Class0 = Class(function () {
-	this.init = function (opts) {};
+		var obj1 = {
+			x: 0,
+			y: 0
+		};
 
-	var b = bind;
-	this.reset = function(fn, ctx) {
-		var f = b(ctx, fn);
-		for (var i = 0; i < LOOP_ITERS; i++) {
-			f(i);
-		}
-	};
-});
+		var view2 = new View({
+			parent: GC.app.view,
+			width: 20,
+			height: 20
+		});
 
-var Class1 = Class(function () {
-	this.init = function (opts) {};
+		var obj2 = {
+			x: 0,
+			y: 0
+		};
 
-	this.reset = function(fn, ctx) {
-		var f = fn.bind(ctx);
-		for (var i = 0; i < LOOP_ITERS; i++) {
-			f(i);
-		}
-	};
-});
-
-var Class2 = Class(function () {
-	this.init = function (opts) {};
-
-	this.reset = function(fn, ctx) {
-		var f = function(i) { fn.call(ctx, i); };
-		for (var i = 0; i < LOOP_ITERS; i++) {
-			f(i);
-		}
-	};
-});
-
-var Class3 = Class(function () {
-	this.init = function (opts) {};
-
-	this.reset = function(fn, ctx) {
-		for (var i = 0; i < LOOP_ITERS; i++) {
-			fn.call(ctx, i);
-		}
-	};
-});
-
-var inst0 = new Class0();
-var inst1 = new Class1();
-var inst2 = new Class2();
-var inst3 = new Class3();
-
-var TestView = exports = Class(BaseTestView, function(supr) {
-	this.init = function(opts) {
 		opts.hideViews = false;
 		opts.disableLogs = true;
 		opts.useMicroseconds = true;
 
-		opts.setupFunc = function() {};
+		opts.setupFunc = function () {};
 
-		opts.testIters = 50;
+		opts.testIters = 10000;
 
 		opts.testFuncs = [
-			function() {
-				inst0.reset(function(i) {
-					return i * i;
-				}, this);
+			function () {
+				animate1(view1)
+					.now({ x: 2500, y: 2500 }, 50, animate1.easeIn)
+					.then({ x: 0, y: 0 }, 50, animate1.easeOut)
+					.then(function() {});
+
+				animate1(obj1)
+					.now({ x: 2500, y: 2500 }, 50, animate1.easeIn)
+					.then({ x: 0, y: 0 }, 50, animate1.easeOut)
+					.then(function() {});
 			},
-			function() {
-				inst1.reset(function(i) {
-					return i * i;
-				}, this);
-			},
-			function() {
-				inst2.reset(function(i) {
-					return i * i;
-				}, this);
-			},
-			function() {
-				inst3.reset(function(i) {
-					return i * i;
-				}, this);
+			function () {
+				animate2(view2)
+					.now({ x: 2500, y: 2500 }, 50, animate2.easeIn)
+					.then({ x: 0, y: 0 }, 50, animate2.easeOut)
+					.then(function() {});
+
+				animate2(obj2)
+					.now({ x: 2500, y: 2500 }, 50, animate2.easeIn)
+					.then({ x: 0, y: 0 }, 50, animate2.easeOut)
+					.then(function() {});
 			}
 		];
 
